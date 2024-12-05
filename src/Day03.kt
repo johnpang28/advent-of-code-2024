7a96ea800def70bd7da2af668f65241bce729734
John Pang <john@peathree.com>
2024-12-05 21:14:08 +0000

    day 3

---
+++ src/Day03.kt
@@ -0,0 +1,54 @@
+import Day03.Do
+import Day03.Dont
+import Day03.Mul
+import Day03.Op
+import Day03.parseLines
+
+object Day03 {
+
+    sealed interface Op
+    data class Mul(val a: Int, val b: Int) : Op
+    data object Do : Op
+    data object Dont : Op
+
+    private val pattern = """mul\(([0-9]{1,3}),([0-9]{1,3})\)|(do\(\))|(don't\(\))""".toRegex()
+
+    fun parseLines(input: List<String>): List<Op> = input.flatMap { parseLine(it) }
+
+    private fun parseLine(input: String): List<Op> =
+        pattern.findAll(input).map {
+            val (a, b, doo, dont) = it.destructured
+            when {
+                doo != "" -> Do
+                dont != "" -> Dont
+                else -> Mul(a.toInt(), b.toInt())
+            }
+        }.toList()
+}
+
+fun main() {
+
+    fun part1(ops: List<Op>): Int =
+        ops.fold(0) { acc, next ->
+            when (next) {
+                is Mul -> acc + next.a * next.b
+                else -> acc
+            }
+        }
+
+    fun part2(ops: List<Op>): Int {
+        val initial: Pair<Op, Int> = Do to 0
+        val (_, sum) = ops.fold(initial) { (doOrDont, x), op ->
+            when (op) {
+                Do -> Do to x
+                Dont -> Dont to x
+                is Mul -> if (doOrDont == Do) doOrDont to x + op.a * op.b else doOrDont to x
+            }
+        }
+        return sum
+    }
+
+    val ops = parseLines(readInput("Day03"))
+    part1(ops).println()
+    part2(ops).println()
+}
